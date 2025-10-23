6a9e188074892af38e4ef8ec748d4f26ccdbb991
Willyan Gomes <willyangomes86@gmail.com>
2025-10-23 10:21:19 -0300

    Ajustes

--- setup/settings.py
+++ setup/settings.py
@@ -1,6 +1,9 @@
 import os
 from pathlib import Path
 from decouple import config  # type: ignore
+from dotenv import load_dotenv
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
